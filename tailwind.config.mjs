12a8eff2fdeb44cdbe64ceddf583456f06649455
Drikus Roor <drikusroor@gmail.com>
2024-01-21 13:47:03 +0100

    config(tailwind): Add custom colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'navy-blue': '#42527d',
+				'sky-blue': '#e2e6ee',
+				'sand-gold': '#f6c195',
+				'copper-orange': '#f0a63f',
+			},
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
